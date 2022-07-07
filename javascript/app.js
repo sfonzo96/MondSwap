45c4092741258a9af1cedb80950e10a608aaf0b3
santif <santiagofonzo@live.com>
2022-07-07 20:21:08 -0300

    Site upgrade + interactive JS
    - Avance con estructura y estilos del sitio.
    - Primera interaccion de JS con DOM para listado de monedas.

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,10 @@
-let fromList = document.querySelector('#fromList');
-let toList = document.querySelector('#toList');
-
+let fromCoinList = document.querySelector('#fromCoinList');
+let toCoinList = document.querySelector('#toCoinList');
 
 class Currency {
-    constructor(ticker,balance,price) {
+    constructor(ticker,name,balance,price) {
         this.ticker = ticker;
+        this.name = name;
         this.balance = balance;
         this.price = price;
     }
@@ -18,5 +18,28 @@ let criptocurrencies = [
     new Currency('DOT',0, 6.90)
 ]
 
+listCurrencies(fromCoinList);
+listCurrencies(toCoinList);
 
+
+function listCurrencies (element) {
+    criptocurrencies.forEach (currencies => {
+        let optnElem = document.createElement('option')
+        optnElem.textContent = currencies.ticker;
+        element.appendChild(optnElem);
+    })
+}
+
+/* 
+FUNCION PARA CONVERTIR Y REDEFINIR BALANCES
+
+Posible forma de calculo para conversiion: usar if (originCoin == 'usdt' && destinyCoin == 'btc') {swapOk(usdtVal,btcVal,swapAmount)} else if {} repetir para todas las posibilidades...
+
+
+function swapOk(originVal, destinyVal, swapAmount) {
+    Muy a lo bruto, tengo que hacer este calculo. Pensar como vincular los valores y los balances en funcion de lo que se ingresa al promp para no repetir tanto codigo.
+    balanceDestino = balanceDestino + swapAmount * valorOrigen / valorDestino;
+    balanceOrigen = balanceOrigen - swapAmount;
+} 
+ */
 
