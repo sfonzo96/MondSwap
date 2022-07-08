2d4bcbbc9ea83bead9ddeebdc2399f9ff6094c37
santif <santiagofonzo@live.com>
2022-07-08 19:24:05 -0300

    Swap working (not final version)
    - Funcion de swap (alpha) en funcionamiento sin validaciones.

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,9 @@
 let fromCoinList = document.getElementById('fromCoinList');
 let toCoinList = document.getElementById('toCoinList');
 let form = document.getElementById('form');
-let fromAmount = document.getElementById('fromAmount');
-let toAmount = document.getElementById('toAmount');
-let fromSelected, toSelected;
+let fromAmountInput = document.getElementById('fromAmount');
+let toAmountInput = document.getElementById('toAmount');
+let fromSelected, toSelected, fromAmount, toAmount, fromPrice, toPrice;
 
 class Currency {
     constructor(ticker,name,balance,price) {
@@ -16,28 +16,29 @@ class Currency {
 
 let criptocurrencies = [
     new Currency('USDT', 'Tether USD', 100, 1), 
-    new Currency('BTC','Bitcoin', 0, 20150), 
-    new Currency('ETH','Ethereum', 0, 1130), 
-    new Currency('ADA','Cardano', 0, 0.46), 
-    new Currency('DOT','Polkador', 0, 6.90)
+    new Currency('BTC', 'Bitcoin', 0, 20150), 
+    new Currency('ETH', 'Ethereum', 0, 1130), 
+    new Currency('ADA', 'Cardano', 0, 0.46), 
+    new Currency('DOT', 'Polkadot', 0, 6.90)
 ] //Funciona OK
 
 function listCurrencies (element) {
-    criptocurrencies.forEach (currencies => {
+    criptocurrencies.forEach (currency => {
         let optnElem = document.createElement('option')
-        optnElem.value = currencies.ticker;
-        optnElem.textContent = currencies.name;
+        optnElem.value = currency.ticker;
+        optnElem.textContent = currency.name;
+        optnElem.setAttribute('price', currency.price);
         element.appendChild(optnElem);
     })
 } //Funciona OK
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    form.addEventListener('submit', submitForm);
+    form.addEventListener('submit', submitForm); //Funciona OK
     fromCoinList.addEventListener('change', function () {getValue(fromCoinList)}); //Funciona OK
     toCoinList.addEventListener('change', function () {getValue(toCoinList)}); //Funciona OK
 });
 
-function getValue(selectedList) { //Funciona OK
+function getValue(selectedList) { //Funciona OK // Validar para que no tome como valor el default del elemento select sino el de los options (childs)
     if (selectedList == fromCoinList) {
         fromSelected = selectedList.value;
         console.log(fromSelected); 
@@ -47,30 +48,32 @@ function getValue(selectedList) { //Funciona OK
     }
 }
 
-function submitForm(e) {
+function submitForm(e) { //Funciona OK
     e.preventDefault();
 
-    calcularSwap() //Crear funcion, ver abajo. 
+    calcularSwap()
 }
 
-function calcularSwap() {
-    
+function calcularSwap() { //Funcion OK
+    fromAmount = fromAmountInput.value; //Funciona OK
+    console.log(fromAmount);
+    criptocurrencies.forEach (currency => {
+        if (fromSelected == currency.ticker) {
+          fromPrice = currency.price;  
+        }
+    })
+    criptocurrencies.forEach (currency => {
+        if (toSelected == currency.ticker) {
+          toPrice = currency.price;  
+        }
+    })
+    toAmount = fromAmount * fromPrice / toPrice;
+    toAmountInput.value = toAmount; 
 }
-
-/* 
-FUNCION calcularSwap PARA CONVERTIR Y REDEFINIR BALANCES
-
-Ver como en funcion de los strings obtenidos en fromSelected y toSelected (ver funcion getText),
-tomando las cantidades ingresadas en los inputs toAmount y fromAmount  + los precios (tomados de la array criptocurrencies {Ver validez en agregar atributo precio a cada opt de la lista para facilitad})
-
-Posible forma de calculo para conversiion: usar if (originCoin == 'usdt' && destinyCoin == 'btc') {swapOk(usdtVal,btcVal,swapAmount)} else if {} repetir para todas las posibilidades...
-
-function swapOk(originVal, destinyVal, swapAmount) {
-    Muy a lo bruto, tengo que hacer este calculo. Pensar como vincular los valores y los balances en funcion de lo que se ingresa al promp para no repetir tanto codigo.
-    balanceDestino = balanceDestino + swapAmount * valorOrigen / valorDestino;
-    balanceOrigen = balanceOrigen - swapAmount;
-} 
-*/
 
 listCurrencies(fromCoinList);
 listCurrencies(toCoinList);
+
+// Pendientes:
+// Desarrollar faucet y sistemma de balances.
+// Desarrollar sistema staking.
