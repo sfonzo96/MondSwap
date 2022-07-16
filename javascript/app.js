3188bf06888b097739092c010704fc80cae3e251
santif <santiagofonzo@live.com>
2022-07-15 23:04:00 -0300

    JavaScript ugrade
    - Calcula automaticamente sin necesidad de apretar boton
    - Al clickear y confirmar se ejecuta el cambio en los balances

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
 let fromCoinList = document.getElementById('fromCoinList');
 let toCoinList = document.getElementById('toCoinList');
 let form = document.getElementById('form');
-let fromAmountInput = document.getElementById('fromAmount');
-let toAmountInput = document.getElementById('toAmount');
+let fromAmountInput = document.getElementById('fromAmountInput');
+let toAmountInput = document.getElementById('toAmountInput');
 let fromSelected, toSelected, fromAmount, toAmount, fromPrice, toPrice;
 
 class Currency {
@@ -15,7 +15,7 @@ class Currency {
 } //Funciona OK
 
 let criptocurrencies = [
-    new Currency('USDT', 'Tether USD', 100, 1), 
+    new Currency('USDT', 'Tether USD', 1000, 1), 
     new Currency('BTC', 'Bitcoin', 0, 20150), 
     new Currency('ETH', 'Ethereum', 0, 1130), 
     new Currency('ADA', 'Cardano', 0, 0.46), 
@@ -42,11 +42,55 @@ function getValue(selectedList) { //Funciona OK // Validar para que no tome como
     }
 }
 
-function submitForm(e) { //Funciona OK
+function executeSwap(e) { //Funciona OK
     e.preventDefault();
 
-    calcularSwap()
+    if (confirm(`You are swapping ${fromAmount} ${fromSelected} for ${toAmount} ${toSelected}. Are you sure about it?`)) {
+        for (const currency of criptocurrencies) {
+            if (fromSelected == currency.ticker) {
+                if (currency.balance < fromAmount || fromAmount < 0) {
+                    alert("You can not swap more than you have! Or you've introduced a negative value"); 
+                    return; 
+                } else {
+                    currency.balance -= fromAmount;
+                    console.log(currency.balance);
+                }
+            }
+        }
+
+        for (const currency of criptocurrencies) { 
+            if (toSelected == currency.ticker) {
+                console.log(`${toAmount}`);
+                currency.balance += toAmount;
+                console.log(currency.balance);
+            }
+        }
+    }
+
+    /* if (confirm(`You are swapping ${fromAmount} ${fromSelected} for ${toAmount} ${toSelected}. Are you sure about it?`)) {
+        criptocurrencies.forEach (currency => {
+            if (fromSelected == currency.ticker) {
+                if (currency.balance < fromAmount || fromAmount < 0) {
+                    alert("You can not swap more than you have! Or you've introduced a negative value"); //Hay que hacer esta validacion más fachera
+                    return; // Ejecuta el alert bien pero no retorna y la funcion sigue corriendo
+                } else { //No se ejecuta si entra al if anterior (OK)
+                    currency.balance -= fromAmount;
+                    console.log(currency.balance);
+                }
+            }
+        })
+
+        criptocurrencies.forEach (currency => { //Se ejecuta de todas formas. Por que? Porque el return que puse solo se retorna a la callback para ESA iteracion, por tanto sigue iterando y ejecutando el codigo.
+            if (toSelected == currency.ticker) {
+                console.log(`${toAmount}`);
+                currency.balance += toAmount;
+                console.log(currency.balance);
+            }
+        })
+    } */
 }
+
+
 
 function calcularSwap() { //Funcion OK
     fromAmount = fromAmountInput.value; //Funciona OK
@@ -63,19 +107,21 @@ function calcularSwap() { //Funcion OK
         }
     })
     toAmount = fromAmount * fromPrice / toPrice;
-    toAmountInput.value = toAmount; 
+    toAmountInput.value = toAmount;
+    console.log(toAmount);
 }
 
 listCurrencies(fromCoinList);
 listCurrencies(toCoinList);
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    form.addEventListener('submit', submitForm); //Funciona OK
-    fromCoinList.addEventListener('change', function () {getValue(fromCoinList)}); //Funciona OK
-    toCoinList.addEventListener('change', function () {getValue(toCoinList)}); //Funciona OK
+    form.addEventListener('submit', executeSwap); //Funciona OK
+    fromCoinList.addEventListener('change', () => {getValue(fromCoinList)}); //Funciona OK
+    toCoinList.addEventListener('change', () => {getValue(toCoinList)}); //Funciona OK
+    fromAmountInput.addEventListener('input', calcularSwap); //Funciona OK
 });
 
 // Pendientes:
-// Desarrollar faucet y sistemma de balances.
+// Desarrollar faucet (timeout y 100 usdt c/24hsq) y sistemma de balances.
 // Desarrollar sistema staking.
-// Automatizar mostrar el valor de la conversion (ejecutar calcularSwap ante el evento de ingresar un value en el fromAmountInput) y posteriormente vincular el evento submit a el cambio en los balances (con previo confirm).
+// Vincular submit al sistema de balances.
